36b31f5a51b72cc5ef75fad56216766b214639ab
Carlos Sanchez <csanchez@maestrodev.com>
2013-04-19 00:39:42 +0200

    Use librarian gem
    Move librarian changes to monkey patches so we can use their latest gem

--- lib/librarian/puppet/extension.rb
+++ lib/librarian/puppet/extension.rb
@@ -7,3 +7,24 @@ module Librarian
 
   end
 end
+
+module Librarian
+
+  # Fixes for librarian not yet released in their gem
+  module Action
+    class Install < Base
+
+    private
+
+      def create_install_path
+        install_path.rmtree if install_path.exist? && destructive?
+        install_path.mkpath
+      end
+
+      def destructive?
+        environment.config_db.local['destructive'] == 'true'
+      end
+    end
+  end
+
+end
